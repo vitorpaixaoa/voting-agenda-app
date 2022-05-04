81d795be8ca2c1448f12137b7768f27846eb31f0
Alan Vitor Paixão <alan.brivia@unicesumar.edu.br>
2022-05-04 20:29:36 -0300

    :sparkles: Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {Client as Styletron} from 'styletron-engine-atomic';
+import {Provider as StyletronProvider} from 'styletron-react';
+import {LightTheme, BaseProvider} from 'baseui';
+import Dashboard from './components/dashboard.js'
+import SockJsClient  from 'react-stomp';
+import { SOCKET_URL} from './assets/constant'
+import Swal from 'sweetalert2'
 
+const engine = new Styletron();
 function App() {
+  
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: true,
+    timer: 10000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+  
+  const alertFinishedSession = (name, sim, nao) => {
+    Toast.fire({
+      icon: 'success',
+      title: 'A pauta '+ name + ' foi encerrada com ' + sim + ' votos SIM e ' + nao + ' votos NÃO'
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+      <SockJsClient
+            url={SOCKET_URL}
+            topics={['/topic/group']}
+            onConnect={console.log("Disconnected!")}
+            onDisconnect={console.log("Disconnected!")}
+            onMessage={msg => alertFinishedSession(msg.voteCountingName, msg.sim, msg.nao)}
+            debug={false}
+        />
+        <Dashboard />
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
-
 export default App;
